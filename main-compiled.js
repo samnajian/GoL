b9df7a92aedca16bd6907d5b54d2b0e2f3059f90
Sam Najian <dev.samnajian@gmail.com>
2016-08-01 19:12:40 +0430

    make it tick correctly

--- main-compiled.js
+++ main-compiled.js
@@ -16,14 +16,6 @@
                 state = true;
             }
 
-            if ([11].indexOf(y) !== -1 && [8].indexOf(x) !== -1) {
-                state = true;
-            }
-
-            if ([12, 13, 14].indexOf(y) !== -1 && [7].indexOf(x) !== -1) {
-                state = true;
-            }
-
             node.style.width = cell_width + "px";
             node.style.height = cell_width + "px";
             node.style.display = "inline-block";
@@ -36,13 +28,89 @@
             if (state) node.style.backgroundColor = alive_color;else node.style.backgroundColor = dead_color;
         })();
 
+        return {
+            state: state,
+            node: node
+        };
+    }
+
+    function Grid(cells) {
+        let grid = doc.querySelector("#grid"),
+            cell_width = 10,
+            dead_color = "#ccc",
+            alive_color = "green",
+            next_gen;
+
+        /**
+         * Populates cells array
+         *
+         * @param width
+         * @param height
+         */
+        const init_cells = (width, height) => {
+            width = width / cell_width;
+            height = height / cell_width;
+
+            for (let y = 0; y < width; y++) {
+                for (let x = 0; x < height; x++) {
+                    cells[x] = cells[x] || [];
+                    cells[x][y] = new Cell(x, y);
+                }
+            }
+        };
+
+        /**
+         * Initial draw of grid with default alive cells
+         *
+         * @param width
+         * @param height
+         */
+        const initial_draw = (width, height) => {
+            grid.innerHTML = "";
+            grid.style.width = width + "px";
+            grid.style.height = height + "px";
+
+            for (let x = 0; x < cells.length; x++) {
+                for (let y = 0; y < cells.length; y++) {
+                    grid.appendChild(cells[x][y].node);
+                }
+            }
+        };
+
+        /**
+         * Updates cells and then the DOM
+         *
+         */
+        const update = () => {
+            grid.innerHTML = ""; // remove prev state from DOM
+            next_gen = [];
+            for (let y = 0; y < cells.length; y++) {
+                for (let x = 0; x < cells.length; x++) {
+                    grid.appendChild(update_cell(x, y).node);
+                }
+            }
+
+            cells = next_gen;
+        };
+
+        /**
+         * Initializes the grid
+         *
+         * @param width
+         * @param height
+         */
+        const init = (width, height) => {
+            init_cells(width, height);
+            initial_draw(width, height);
+        };
+
         /**
          * Counts live neighbors
          *
          * @param cells
          * @returns {number}
          */
-        const count_alive_neighbors = cells => {
+        const count_alive_neighbors = (x, y) => {
             let count = 0,
                 x_count = cells.length - 1; //  given cell height and width are the same this should work for the y as well
 
@@ -79,8 +147,9 @@
          * @param cells
          * @returns {boolean}
          */
-        const get_new_state = cells => {
-            let count = count_alive_neighbors(cells);
+        const get_new_state = (x, y, cell) => {
+            let count = count_alive_neighbors(x, y),
+                state = cell.state;
 
             if (state && count < 2) //  Live cell with less than 2 live cells
                 state = false;
@@ -103,86 +172,14 @@
          * @param cells
          * @returns {update}
          */
-        const update = cells => {
+        const update_cell = (x, y) => {
+            next_gen[x] = next_gen[x] || [];
+            next_gen[x][y] = Cell(x, y);
 
-            if (get_new_state(cells)) node.style.backgroundColor = alive_color;else node.style.backgroundColor = dead_color;
+            next_gen[x][y].state = get_new_state(x, y, next_gen[x][y]);
+            if (next_gen[x][y].state) next_gen[x][y].node.style.backgroundColor = alive_color;else next_gen[x][y].node.style.backgroundColor = dead_color;
 
-            return {
-                node: node,
-                state: state
-            };
-        };
-
-        return {
-            state: state,
-            update: update,
-            node: node
-        };
-    }
-
-    function Grid(cells) {
-        let grid = doc.querySelector("#grid"),
-            cell_width = 10;
-
-        /**
-         * Populates cells array
-         *
-         * @param width
-         * @param height
-         */
-        const init_cells = (width, height) => {
-            width = width / cell_width;
-            height = height / cell_width;
-
-            for (let w = 0; w < width; w++) {
-                for (let h = 0; h < height; h++) {
-                    cells[w] = cells[w] || [];
-                    cells[w][h] = new Cell(w, h);
-                }
-            }
-        };
-
-        /**
-         * Initial draw of grid with default alive cells
-         *
-         * @param width
-         * @param height
-         */
-        const initial_draw = (width, height) => {
-            grid.innerHTML = "";
-            grid.style.width = width + "px";
-            grid.style.height = height + "px";
-
-            for (let x = 0; x < cells.length; x++) {
-                for (let y = 0; y < cells.length; y++) {
-                    grid.appendChild(cells[x][y].node);
-                }
-            }
-        };
-
-        /**
-         * Updates cells and then the DOM
-         *
-         */
-        const update = () => {
-            grid.innerHTML = ""; // remove prev state from DOM
-
-            for (let x = 0; x < cells.length; x++) {
-                for (let y = 0; y < cells.length; y++) {
-                    grid.appendChild(cells[x][y].update(cells).node);
-                }
-            }
-        };
-
-        /**
-         * Initializes the grid
-         *
-         * @param width
-         * @param height
-         */
-        const init = (width, height) => {
-            init_cells(width, height);
-            initial_draw(width, height);
+            return next_gen[x][y];
         };
 
         return {
